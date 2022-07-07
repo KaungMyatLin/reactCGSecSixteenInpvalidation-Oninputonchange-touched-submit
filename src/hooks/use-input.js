e2094c2bcacb80df779b64d3e6a5ef93b11a7daf
John Myat Lin <kaungmyatlin.worklife@gmail.com>
2022-07-07 15:11:42 +0630

    updating

--- src/hooks/use-input.js
+++ src/hooks/use-input.js
@@ -1,13 +1,13 @@
 import { useState } from "react";
 
-const useInput = (validateVal) {
+const useInput = (validateVal) => {
     // copy states, then inferred state, then argument, then return, then functions.
     const [enteredVal, setenteredVal] = useState('');
     const [isTouched, setisTouched] = useState(false);
 
     // inferred state.
     const inpValisValid = validateVal(enteredVal);
-    const hasError = !inpValisValid && enteredNTouched;
+    const hasError = !inpValisValid && isTouched;
 
     const inpValChangeHandler = event => {
         setenteredVal(event.target.value);
