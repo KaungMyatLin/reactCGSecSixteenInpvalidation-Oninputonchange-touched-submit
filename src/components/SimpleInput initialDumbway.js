5267ec7ac264fe668360854dc8c0f9d86f006d8a
John Myat Lin <kaungmyatlin.worklife@gmail.com>
2022-07-07 14:25:33 +0630

    first commit

---
+++ src/components/SimpleInput initialDumbway.js
@@ -0,0 +1,60 @@
+import { useState } from "react";
+
+const SimpleInput = (props) => {
+  const [enteredN, setEnteredN] = useState('');
+  const [enteredNisValid, setEnteredNisValid] = useState(false);
+  const [enteredNTouched, setenteredNTouched] = useState(false);
+
+  const nameInpChangeHandler = event => {
+    setEnteredN(event.target.value);
+    if (event.target.value.trim() !== '') {
+      setEnteredNisValid(true);
+      return;
+    }
+    setEnteredNisValid(false);
+  }
+  const nameInputBlurHandler = event => {
+    setenteredNTouched(true);
+
+    if (enteredN.trim() === '') {
+      setEnteredNisValid(false);
+      return;
+    }
+  }
+
+  const formSubHandler = event => {
+    event.preventDefault();
+
+    setenteredNTouched(true);
+
+    if (enteredN.trim() === '') {
+      setEnteredNisValid(false);
+      return;
+    }
+    setEnteredNisValid(true);
+
+    setEnteredN('');
+  }
+
+  const nameInpIsInvalid = !enteredNisValid && enteredNTouched;
+
+  const nameInputClasses = nameInpIsInvalid? 'form-control invalid': 'form-control'
+
+  return (
+    <form onSubmit={formSubHandler}>
+      <div className={nameInputClasses}>
+        <label htmlFor='name'>Your Name</label>
+        <input type='text' id='name' 
+        onChange={nameInpChangeHandler}
+        onBlur={nameInputBlurHandler}
+        value={enteredN}/>
+        {nameInpIsInvalid && <p className="error-text">Not empty</p>}
+      </div>
+      <div className="form-actions">
+        <button>Submit</button>
+      </div>
+    </form>
+  );
+};
+
+export default SimpleInput;
---
+++ src/components/SimpleInput.js
@@ -0,0 +1,60 @@
+import { useState } from "react";
+
+const SimpleInput = (props) => {
+  const [enteredN, setEnteredN] = useState('');
+  const [enteredNTouched, setenteredNTouched] = useState(false);
+
+  // First, get rid of enteredNisValid state and make constant if you came from 'SimpleInput initialDumbway.js'
+  const enteredNisValid = enteredN.trim() !== '';
+  const nameInpIsInvalid = !enteredNisValid && enteredNTouched;
+  // get rid of all setEnteredNisValid() codes.
+
+  let formIsValid = false; 
+
+  if (enteredNisValid) {
+    formIsValid = true;
+  }
+  else {
+    formIsValid = false;
+  }
+
+  const nameInpChangeHandler = event => {
+    setEnteredN(event.target.value);
+  }
+  const nameInputBlurHandler = event => {
+    setenteredNTouched(true);
+  }
+
+  const formSubHandler = event => {
+    event.preventDefault();
+
+    setenteredNTouched(true);
+
+    if(!enteredNisValid) {
+      return;
+    }
+
+    setEnteredN('');
+    setenteredNTouched(false);
+  }
+
+  const nameInputClasses = nameInpIsInvalid? 'form-control invalid': 'form-control'
+
+  return (
+    <form onSubmit={formSubHandler}>
+      <div className={nameInputClasses}>
+        <label htmlFor='name'>Your Name</label>
+        <input type='text' id='name' 
+        onChange={nameInpChangeHandler}
+        onBlur={nameInputBlurHandler}
+        value={enteredN}/>
+        {nameInpIsInvalid && <p className="error-text">Not empty</p>}
+      </div>
+      <div className="form-actions">
+        <button disabled={!formIsValid}>Submit</button>
+      </div>
+    </form>
+  );
+};
+
+export default SimpleInput;
