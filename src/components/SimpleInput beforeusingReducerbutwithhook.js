d7048e424785fb44fdcb50e967edbdd572d776b9
KML <kaungmyatlin.worklife@gmail.com>
2022-09-27 18:28:24 +0630

    update

--- src/components/SimpleInput beforeusingReducerbutwithhook.js
+++ src/components/SimpleInput beforeusingReducerbutwithhook.js
@@ -1,79 +1,72 @@
-import useInput from "../hooks/useInputHookBeforeReducer";
-
+import useInput from '../hooks/useInputHookBeforeReducer'
 const SimpleInput = () => {
-  const {
-    val: enteredN,
-    hasError: nameInpHasError,
-    isValid: enteredNisValid,
-    inpValChangeHandler: nameInpChangeHandler,
-    inpValBlurHandler: nameInputBlurHandler,
-    reset: resetNInpAndTouched,
-  } = useInput((value) => value.trim() !== "");
+    const {value: entEmail
+        , entValIsValid: emailValIsValid
+        , inpFiIsWrong: emailInpFiIsWrong
+        , inpChangeHdl: emailInpChangeHdl
+        , inpBlurHdl: emailInpBlurHdl
+        , resetInpFiValXTouched: resetEmInpFiStates
+    } = useInput(value => value.includes('@'))
 
-  const {
-    val: enteredEm,
-    hasError: emInpHasError,
-    isValid: enteredEmisValid,
-    inpValChangeHandler: emInpChangeHandler,
-    inpValBlurHandler: emInputBlurHandler,
-    reset: resetEmInpAndTouched,
-  } = useInput((value) => value.includes("@"));
+    const {value: entPw
+        , entValIsValid: pwValIsValid
+        , inpFiIsWrong: pwInpFiIsWrong
+        , inpChangeHdl: pwInpChangeHdl
+        , inpBlurHdl: pwInpBlurHdl
+        , resetInpFiValXTouched: resetPwInpFiStates
+    } = useInput(value => value.trim() !== '')
 
-  let formIsValid = false;
+    let formIsValid = false;
 
-  if (enteredNisValid && enteredEmisValid) {
-    formIsValid = true;
-  } else {
-    formIsValid = false;
-  }
-
-  const formSubHandler = (event) => {
-    event.preventDefault();
-
-    if (!enteredNisValid) {
-      return;
+    if (emailValIsValid && pwValIsValid) {
+        formIsValid = true;
+    }
+    else {
+        formIsValid = false
     }
 
-    resetNInpAndTouched();
-    resetEmInpAndTouched();
-  };
+    const emInpClasses = emailInpFiIsWrong ? 'form-control invalid' : 'form-control'
+    const pwInpClasses = pwInpFiIsWrong ? 'form-control invalid' : 'form-control'
 
-  const nameInputClasses = nameInpHasError
-    ? "form-control invalid"
-    : "form-control";
-  const emInputClasses = emInpHasError
-    ? "form-control invalid"
-    : "form-control";
+    const formSubHandler = (e) => {
+        e.preventDefault();
+        if (!emailValIsValid) {
+            return
+        }
+        resetEmInpFiStates();
+        resetPwInpFiStates();
+    }
 
-  return (
-    <form onSubmit={formSubHandler}>
-      <div className={nameInputClasses}>
-        <label htmlFor="name">Your Name</label>
-        <input
-          type="text"
-          id="name"
-          onChange={nameInpChangeHandler}
-          onBlur={nameInputBlurHandler}
-          value={enteredN}
-        />
-        {nameInpHasError && <p className="error-text">Not empty</p>}
-      </div>
-      <div className={emInputClasses}>
-        <label htmlFor="name">Your Name</label>
-        <input
-          type="text"
-          id="name"
-          onChange={emInpChangeHandler}
-          onBlur={emInputBlurHandler}
-          value={enteredEm}
-        />
-        {emInpHasError && <p className="error-text">Not empty</p>}
-      </div>
-      <div className="form-actions">
-        <button disabled={!formIsValid}>Submit</button>
-      </div>
-    </form>
-  );
-};
 
-export default SimpleInput;
+    return (
+        <form onSubmit={formSubHandler}>
+            <div className={emInpClasses}>
+                <label htmlFor="name">Email</label>
+                <input
+                type="text"
+                id="name"
+                onChange={emailInpChangeHdl}
+                onBlur={emailInpBlurHdl}
+                value={entEmail}
+                />
+                {emailInpFiIsWrong && <p className="error-text">Must includes '@'</p>}
+            </div>
+            <div className={pwInpClasses}>
+                <label htmlFor="name">Password</label>
+                <input
+                type="text"
+                id="name"
+                onChange={pwInpChangeHdl}
+                onBlur={pwInpBlurHdl}
+                value={entPw}
+                />
+                {pwInpFiIsWrong && <p className="error-text">Must not empty</p>}
+            </div>
+            <div className="form-actions">
+                <button disabled={!formIsValid}>Submit</button>
+            </div>
+        </form>
+    )
+}
+
+export default SimpleInput
